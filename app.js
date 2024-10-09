83340c6dfe06b23b4e8b9d734503f898ea92000d
SaurabhKuntal110 <wintersoldier110@gmail.com>
2024-10-09 09:22:42 +0530

    Access Message Container and Add showWinner function

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn= document.querySelector("#new-btn");
+let msgContainer= document.querySelector(".msg-container");
+let msg= document.querySelector("#msg");
+
 let turnO = true; //playerX,playerO if turnO is true then playerO turn else playerX turn
 let winPatterns= [
     [0,1,2],
@@ -29,6 +33,10 @@ boxes.forEach((box)=>{
     })
 });
 
+const showWinner= (winner) =>{
+    msg.innerText=`Congratulations!! Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+}
 const checkWinner = () => {
     for( let pattern of winPatterns){
         let pos1Val =    boxes[pattern[0]].innerText;
@@ -37,7 +45,7 @@ const checkWinner = () => {
 
         if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
             if(pos1Val===pos2Val && pos2Val === pos3Val){
-                console.log("Winner", pos1Val);
+                showWinner(pos1Val);
             }
         }
     }
